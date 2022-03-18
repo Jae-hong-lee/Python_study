23d467a7b3768163c2a4024e7a2eec739ba5dbcc
Jae-hong-lee <72030487+Jae-hong-lee@users.noreply.github.com>
2022-03-19 00:53:09 +0900

    Add files via upload

--- programmers/2022/0319/03192.py
+++ programmers/2022/0319/03192.py
@@ -4,22 +4,94 @@
 
 def solution(s):
     answer = []
-    # cnt = 0
-    s_list = list(s)
-    # print(s_list)
+    cnt = 0
     for i in range(len(s)):
         stack = []
-        # 스택 초기화O ,돌리는것. X
-        for j in range(len(s)):
-            if not stack: 
-                stack.append(s_list[j])
-            else:
-                if stack[-1] == '(' :
+        for j in s:
+            if j == '{'or j=='[' or j=='(':
+                stack.append(j)
+            # Exception has occurred: IndexError
+            # list index out of range
+            elif len (stack) > 0:
+                if stack[-1] == '{' and j =='}':
                     stack.pop()
-    return answer
+                elif stack[-1] == '[' and j ==']' :
+                    stack.pop()
+                elif stack[-1] == '(' and j ==')' :
+                    stack.pop()
+            else:
+                stack.append(j)
+        if len(stack)== 0: cnt+=1
 
+        x = s[0]
+        s = s[1:]+x
+    
+    return cnt
 print(solution("[](){}")) # result 3
-print(solution("[)(]"))   # result 0        
+print(solution("[)(]"))   # result 0    
+# 통과 ㅠㅠ
+
+# def solution(s):
+#     answer = []
+#     cnt = 0
+#     # x=s[0]
+#     # s=s[1:]+x
+#     # print(s)
+#     for i in range(len(s)):
+#         stack = []
+#         for j in s:
+#             if j == '{'or j=='[' or j=='(':
+#                 stack.append(j)
+#             # Exception has occurred: IndexError
+#             # list index out of range
+#             elif stack[-1] == '{' and j =='}':
+#                 stack.pop()
+#             elif stack[-1] == '[' and j ==']':
+#                 stack.pop()
+#             elif stack[-1] == '(' and j ==')':
+#                 stack.pop()
+        
+#         if len(stack)== 0: cnt+=1
+
+#         x = s[0]
+#         s = s[1:]+x
+#         # 뒤로 돌리고
+#     answer.append(cnt)
+#     return answer
+
+# print(solution("[](){}")) # result 3
+# print(solution("[)(]"))   # result 0     
+
+# cnt 를 어디서 추가 시켜야할까, s 슬라이싱해서 뒤로 보내기.
+# def solution(s):
+#     answer = []
+#     cnt = 0
+#     s_list = list(s)
+#     # print(s_list)
+#     for i in range(len(s)):
+#         stack = []
+#         # cnt +=1
+#         # 스택 초기화O ,돌리는것. X
+#         for j in s:
+#             if j == '{'or '[' or'(':
+#                 stack.append(j)
+#             # 스택에 열린 괄호들 이면 넣고 비교하기
+#             if stack[-1] == '{' and j =='}':
+#                 stack.pop()
+#             if stack[-1] == '[' and j ==']':
+#                 stack.pop()
+#             if stack[-1] == '(' and j ==')':
+#                 stack.pop()
+#         # answer.append(cnt)
+#         # cnt =0
+#         # stack 값이 있는지 확인.
+#         if not stack:
+#             True
+#         else: False
+#     return answer
+
+# print(solution("[](){}")) # result 3
+# print(solution("[)(]"))   # result 0        
 
 
     # for i in range(len(s)): # 괄호를 한바퀴 도는것, 뒤로 추가 시켜줘야함.
